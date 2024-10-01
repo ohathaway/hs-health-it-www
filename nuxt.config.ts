7f34b90ecfc220a823c5ffc7fc583ed8c09945f3
Owen Hathaway <owen@prsdata.com>
2024-09-30 20:02:26 -0600

    added placeholder favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/img/logo_idea_1.png'
+        }
+      ]
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
